24aaa567abdacc72ec2ff5b9674310aa33d4849d
Chamath Sasmitha <sasmithachamath@gmail.com>
2025-10-02 15:02:59 +0530

    refactored

--- Part 02/phonebook/src/App.jsx
+++ Part 02/phonebook/src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 import Person from './components/Person'
+import PersonForm from './components/PersonForm'
 import { nanoid } from 'nanoid'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: nanoid() },
+    { name: 'Arto Hellas', 
+      number: '39-44-5323523',
+      id: nanoid() },
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const exists = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
 
   const addPerson = (event) => {
     event.preventDefault()
-    
+
     if(exists) {
       alert(`${newName} is already added to phonebook`)
       return
@@ -20,33 +25,35 @@ const App = () => {
 
     const personObject = {
       name: newName,
+      number: newNumber,
       id: nanoid()
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      
+      <PersonForm addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber} 
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange} />
+
       <h2>Numbers</h2>
-      <div>
-          {persons.map(person => 
-            <Person person={person} key={person.id}/>
-          )}
-      </div>
+
+      <Persons persons={persons} />
     </div>
   )
 }
---
+++ Part 02/phonebook/src/components/Persons.jsx
@@ -0,0 +1,13 @@
+import Person from "./Person";
+
+const Persons = ({ persons }) => {
+    return (
+        <div>
+            {persons.map(person =>
+                <Person person={person} key={person.id} />
+            )}
+        </div>
+    )
+}
+
+export default Persons
