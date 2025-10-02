5cf8cf64bc52f93b07713f20882b05af2c26aca6
Chamath Sasmitha <sasmithachamath@gmail.com>
2025-10-02 16:43:13 +0530

    cleanup after removing Filter.jsx

--- Part 02/phonebook/src/App.jsx
+++ Part 02/phonebook/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import Person from './components/Person'
 import PersonForm from './components/PersonForm'
 import { nanoid } from 'nanoid'
 import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      number: '39-44-5323523',
-      id: nanoid() },
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
@@ -44,10 +44,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      
+
+      <h2>Add a new</h2>
+
       <PersonForm addPerson={addPerson}
         newName={newName}
-        newNumber={newNumber} 
+        newNumber={newNumber}
         handleNameChange={handleNameChange}
         handleNumberChange={handleNumberChange} />
 
