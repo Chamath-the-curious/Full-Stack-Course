2614b0dd9402f979653604ed5ce0f2b653b9d8da
Chamath Sasmitha <sasmithachamath@gmail.com>
2025-10-02 17:34:23 +0530

    2.10 done

--- Part 02/phonebook/src/App.jsx
+++ Part 02/phonebook/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import PersonForm from './components/PersonForm'
 import { nanoid } from 'nanoid'
 import Persons from './components/Persons'
+import Filter from './components/Filter'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -12,6 +13,7 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const exists = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
 
@@ -41,9 +43,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    const value = event.target.value
+    setNewFilter(value.toLowerCase())
+  }
+
+  const visiblePersons = newFilter === ''
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(newFilter))
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
 
       <h2>Add a new</h2>
 
@@ -55,7 +68,7 @@ const App = () => {
 
       <h2>Numbers</h2>
 
-      <Persons persons={persons} />
+      <Persons persons={visiblePersons} />
     </div>
   )
 }
